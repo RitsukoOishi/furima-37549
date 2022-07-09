73dabb65166f18022ec1441e5796dbc460c85255
mai-yoshida <hudiezhimu@gmail.com>
2022-07-09 22:44:58 +0900

    アップロード機能
    furimasコントローラーの作成
    商品出品画面の作成

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,4 +1,16 @@
 class FurimasController < ApplicationController
   def index
+    @furimas = Furima.all
   end
+  def new
+    @furima = Furima.new
+  end
+  def create
+    Furima.create(furima_params)
+  end
+
+  private
+  def furima_params
+    params.require(:furima).permit(:image, :title, :content, :genre_id, :condition_id, :fee_id, :prefecture_id, :days_num_id, :price, :user)
+  end    
 end
---
+++ app/models/furima.rb
@@ -0,0 +1,11 @@
+class Furima < ApplicationRecord
+  belongs_to :user
+  validates :image,         presence: true
+  validates :title,         presence: true
+  validates :content,       presence: true
+  validates :condition_id,  presence: true
+  validates :fee_id,        presence: true
+  validates :prefecture_id, presence: true
+  validates :days_num_id,   presence: true
+  validates :price,         presence: true
+end
