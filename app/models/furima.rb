8caaf664aedce5a271d26c12b934eadbec4d85dd
mai-yoshida <hudiezhimu@gmail.com>
2022-07-10 11:50:01 +0900

    配送日数のプルダウンが実装完了。全プルダウンが完了

--- app/models/furima.rb
+++ app/models/furima.rb
@@ -7,14 +7,15 @@ class Furima < ApplicationRecord
   belongs_to :fee
   belongs_to :condition
   belongs_to :prefecture
+  belongs_to :days_num
   validates :image,         presence: true
   validates :title,         presence: true
   validates :content,       presence: true
-  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :days_num_id,   presence: true
+  validates :genre_id,      numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id,  numericality: { other_than: 1 , message: "can't be blank"}
+  validates :fee_id,        numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_num_id,   numericality: { other_than: 1 , message: "can't be blank"}
   validates :price,         presence: true
 
 
