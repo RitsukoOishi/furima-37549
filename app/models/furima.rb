4109f04c462c066471ce96675693ce03b3340ea6
mai-yoshida <hudiezhimu@gmail.com>
2022-07-10 11:35:49 +0900

    配送料(fee)のプルダウンの実装

--- app/models/furima.rb
+++ app/models/furima.rb
@@ -4,14 +4,15 @@ class Furima < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
-  belongs_to :prefecture
+  belongs_to :fee
   belongs_to :condition
+  belongs_to :prefecture
   validates :image,         presence: true
   validates :title,         presence: true
   validates :content,       presence: true
   validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :fee_id,        presence: true
+  validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :days_num_id,   presence: true
   validates :price,         presence: true
